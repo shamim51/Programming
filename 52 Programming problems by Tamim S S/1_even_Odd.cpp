1ce83726c5b910b7ed34e4a8906bcff349036c2e
Shamim Ahmed <shamimahmed30082@gmail.com>
2023-02-27 23:40:57 +0600

    practiced from 52 programming problems by [Tamim Sahariar Subin]

---
+++ 52 Programming problems by Tamim S S/1_even_Odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i = 0; i < n; i++){
+        int x;
+        cin>>x;
+        
+        if(x%2 == 0){
+            cout<<"even"<<endl;
+        }
+        else cout<<"odd"<<endl;
+    }
+}
